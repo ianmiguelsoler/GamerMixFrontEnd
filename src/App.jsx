86a2319079cc005d0fc25628214651edee047e60
ianmiguelsoler <ianmiguel@gmail.com>
2025-02-15 20:42:07 +0100

    add main layout components and routing; implement navigation and structure for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navegacion from './Componentes/estructura/Navegacion.jsx';
+import Cabecera from './Componentes/estructura/Cabecera.jsx';
+import Contenido from './Componentes/estructura/Contenido.jsx';
+import Pie from './Componentes/estructura/Pie.jsx';
+import RutasGamerMix from './Componentes/secciones/rutas/RutasGamerMix.jsx';
 import './App.css'
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
@@ -8,30 +10,21 @@ import "primeicons/primeicons.css";
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+        <div className="ml-[200px] p-6">
+          {/* <ProveedorSesion> */}
+            <Navegacion />
+            <Cabecera />
+                <Contenido>
+                  <RutasGamerMix />
+                </Contenido>
+            <Pie />
+          {/* </ProveedorSesion> */}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
